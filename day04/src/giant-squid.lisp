f91021995c8dfa90616725adb72be59c9b5168ef
Christophe Thibaut <cthibauttof@gmail.com>
2023-11-16 00:10:47 +0100

    create grids day 04

--- day04/src/giant-squid.lisp
+++ day04/src/giant-squid.lisp
@@ -11,3 +11,10 @@
 (defun read-numbers-from-file (file-path)
   (mapcar #'read-numbers (uiop:read-file-lines file-path)))
 
+(defun create-grids (numbers)
+  (if (null numbers)
+    ()
+    (let ((grid-numbers (subseq (cdr numbers) 0 5))
+          (remaining-numbers (subseq numbers 6)))
+      (cons (make-array '(5 5) :initial-contents grid-numbers)
+            (create-grids remaining-numbers)))))
